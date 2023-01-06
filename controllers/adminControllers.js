087c158d94ff2fadb06e27b3e6234d4bff5b8b06
nazimameer <nazimameerpp@gmail.com>
2023-01-06 08:43:29 +0530

    fix last

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -41,34 +41,34 @@ module.exports = {
          
       }else{
 
-        // const deliveredOrders = await orderModel.countDocuments({ orderstatus:"Delivered"})
-        // const shippedOrders = await orderModel.countDocuments({ orderstatus:"Shipped"})
-        // const pendingOrders = await orderModel.countDocuments({ orderstatus:"PENDING"})
-        // const confirmOrders = await orderModel.countDocuments({ orderstatus:"Confirmed"})
-        // const cancelOrders = await orderModel.countDocuments({ orderstatus:"Cancelled"})
-        // const orders = await orderModel.countDocuments({})
-        // const wholestock = await productModel.aggregate([{$group:{_id:'',"stock":{$sum: '$stock' }}}, {$project: {_id: 0,"TotalAmount": '$stock'}}]);
-        // const orderedstock = await orderModel.aggregate([{$unwind:"$productsInfo"},{$group:{_id:'',count:{$sum:'$productsInfo.quantity'}}},{$project:{_id:0,"count":"$count"}}])
-        // const orderpers = (orderedstock[0].count/wholestock[0].TotalAmount)*100;
-        // const sales = await orderModel.aggregate([
-        //     {$group:{_id:'',"total":{$sum:'$total'}}},{$project:{_id:0,'totalsale':'$total'}}
-        //   ])
-        // const totalsale = sales[0].totalsale;
-        // const wholeworth = await productModel.aggregate([{$project:{price:1,stock:1, total:{ $multiply:["$price","$stock"] }}},{$group:{ _id:'',"total":{$sum:"$total"}}},{$project:{_id:0,'wholeprice':"$total"}}])
-        // const wholeworthprice =  wholeworth[0].wholeprice;
-        // console.log(wholeworthprice)
-        // const salespers = (totalsale/wholeworthprice)*100; 
-        // const revenue = (totalsale*30)/100;
-        // const totalrevenue = (wholeworthprice*30)/100;
-        // const revenuepers = (revenue/totalrevenue)*100;
-        // const cost = (totalsale*70)/100;
-        // const totalcost = (wholeworthprice*70)/100;
-        // const costpers = (cost/totalcost)*100;
-        // console.log(totalcost)
+        const deliveredOrders = await orderModel.countDocuments({ orderstatus:"Delivered"})
+        const shippedOrders = await orderModel.countDocuments({ orderstatus:"Shipped"})
+        const pendingOrders = await orderModel.countDocuments({ orderstatus:"PENDING"})
+        const confirmOrders = await orderModel.countDocuments({ orderstatus:"Confirmed"})
+        const cancelOrders = await orderModel.countDocuments({ orderstatus:"Cancelled"})
+        const orders = await orderModel.countDocuments({})
+        const wholestock = await productModel.aggregate([{$group:{_id:'',"stock":{$sum: '$stock' }}}, {$project: {_id: 0,"TotalAmount": '$stock'}}]);
+        const orderedstock = await orderModel.aggregate([{$unwind:"$productsInfo"},{$group:{_id:'',count:{$sum:'$productsInfo.quantity'}}},{$project:{_id:0,"count":"$count"}}])
+        const orderpers = (orderedstock[0].count/wholestock[0].TotalAmount)*100;
+        const sales = await orderModel.aggregate([
+            {$group:{_id:'',"total":{$sum:'$total'}}},{$project:{_id:0,'totalsale':'$total'}}
+          ])
+        const totalsale = sales[0].totalsale;
+        const wholeworth = await productModel.aggregate([{$project:{price:1,stock:1, total:{ $multiply:["$price","$stock"] }}},{$group:{ _id:'',"total":{$sum:"$total"}}},{$project:{_id:0,'wholeprice':"$total"}}])
+        const wholeworthprice =  wholeworth[0].wholeprice;
+        console.log(wholeworthprice)
+        const salespers = (totalsale/wholeworthprice)*100; 
+        const revenue = (totalsale*30)/100;
+        const totalrevenue = (wholeworthprice*30)/100;
+        const revenuepers = (revenue/totalrevenue)*100;
+        const cost = (totalsale*70)/100;
+        const totalcost = (wholeworthprice*70)/100;
+        const costpers = (cost/totalcost)*100;
+        console.log(totalcost)
     
       
     
-        res.render("admin/dashboard",{ admin: true, deliveredOrders:0, shippedOrders:0, pendingOrders:0, confirmOrders:0, cancelOrders:0,orders:0,orderpers:0,totalsale:0,salespers:0,revenue:0,revenuepers:0,cost:0,costpers:0 });
+        res.render("admin/dashboard",{ admin: true, deliveredOrders, shippedOrders, pendingOrders, confirmOrders, cancelOrders,orders,orderpers,totalsale,salespers,revenue,revenuepers,cost,costpers });
       
       }
 
